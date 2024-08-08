dcab55e302e2fc67705a26ba2575d5d87de9c0f6
sonaljain01 <jainsonal8630407466@gmail.com>
2024-08-08 10:54:01 +0530

    Create WebPages for Real Estate comapny

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    alert(`Information saved successfully`);
+
+    form.reset();
+});
